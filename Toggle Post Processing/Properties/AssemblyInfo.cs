47393bd0a3bcdf1a2438b550b66268dcddbd56fc
Arion-Kun <Arion-Kun@users.noreply.github.com>
2020-09-09 17:18:58 +0200

    Upload

---
+++ Toggle Post Processing/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Toggle_PostProcessing;
+using BuildInfo = Toggle_PostProcessing.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.Name)]
+[assembly: AssemblyDescription("A Melon Mod for VRChat that adds the option to disable Post Processing in Worlds.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3359f93-f373-4e70-9788-2e6b95f667ed")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(TogglePostProcessing), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
---
+++ Toggle Post Processing/Toggle Post Processing.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using MelonLoader;
+using UnityEngine.Rendering.PostProcessing;
+
+
+namespace Toggle_PostProcessing
+{
+    public static class BuildInfo
+    {
+        public const string Author = "arion#1223";
+        public const string Company = null;
+        public const string DownloadLink = "https://github.com/Arion-Kun/TogglePostProcessing/releases/";
+        public const string Name = "Toggle Post Processing";
+
+        public const string Version = "1.0.0";
+    }
+    public sealed class TogglePostProcessing : MelonMod
+    {
+        public bool ToggleHandler;
+        public override void OnApplicationStart()
+        {
+            MelonPrefs.RegisterCategory("TogglePostProcessing", "Toggle Post Processing");
+            MelonPrefs.RegisterBool("TogglePostProcessing", "DisablePostProcessing", false, "Disable Post Processing");
+            ToggleHandler = MelonPrefs.GetBool("TogglePostProcessing", "DisablePostProcessing");
+        }
+        public override void OnLevelWasLoaded(int level)
+        {
+            ToggleMethod(ToggleHandler);
+        }
+        public override void OnModSettingsApplied()
+        {
+            ToggleHandler = MelonPrefs.GetBool("TogglePostProcessing", "DisablePostProcessing");
+            ToggleMethod(ToggleHandler);
+        }
+        private static void ToggleMethod(bool value)
+        {
+            PostProcessVolume[] PostProcessingObjects = UnityEngine.Object.FindObjectsOfType<PostProcessVolume>().ToArray();
+            foreach (PostProcessVolume List in PostProcessingObjects)
+            {
+                List.GetComponent<PostProcessVolume>().enabled = !value;
+            }
+        }
+    }
+}
