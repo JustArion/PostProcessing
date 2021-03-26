2b88ddb1a8902aafc550760497af62772226dc8d
arion <53127594+Arion-Kun@users.noreply.github.com>
2021-03-26 05:19:42 +0200

    2.0

---
+++ TogglePostProcessing/Rewrite/QuickMenu.cs
@@ -0,0 +1,47 @@
+﻿using MelonLoader;
+using RubyButtonAPI;
+using UnityEngine;
+using UnityEngine.UI;
+using VRCSDK2;
+
+namespace Dawn.TPP
+{
+    internal static class QuickMenu
+    {
+        private const float QMX = 2.7f;
+        private const float QMY = 1.8f;
+        
+        private static QMToggleButton TPPQM;
+
+        internal static void InitQM()
+        {
+            TPPQM = new QMToggleButton("ShortcutMenu", 1.24f, 1.75f, "TPP", () =>
+            {
+                MelonPreferences.SetEntryValue("TogglePostProcessing", "DisablePostProcessing", false);
+                Core.m_PostProcessing = true;
+                Core.LayerChange();
+            }, "OFF", () =>
+            {
+                MelonPreferences.SetEntryValue("TogglePostProcessing", "DisablePostProcessing", true);
+                Core.m_PostProcessing = false;
+                Core.LayerChange();
+            }, "Toggle Post Processing");
+            TPPQM.btnOff.SetSizeButtonfor(QMX, QMY);
+            TPPQM.btnOn.SetSizeButtonfor(QMX, QMY);
+            TPPQM.getGameObject().SetSizeButtonfor(1.9f, 1.8f);
+            TPPQM.getGameObject().GetComponent<Image>().enabled = false;
+            TPPQM.getGameObject().AddComponent<BoxCollider>();
+            TPPQM.getGameObject().AddComponent<VRC_UiShape>();
+            TPPQM.getGameObject().AddComponent<GraphicRaycaster>();
+            TPPQM.setToggleState(Core.m_PostProcessing);
+            TPPQM.setActive(Core.m_QuickMenu);
+            TPPQM.getGameObject().name = "[Mod] TPP Toggle";
+        }
+
+        internal static void QMPrefsRefresh()
+        {
+            TPPQM.setActive(Core.m_QuickMenu);
+            TPPQM.setToggleState(Core.m_PostProcessing);
+        }
+    }
+}
---
+++ TogglePostProcessing/Rewrite/TPP.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine.Rendering.PostProcessing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using RubyButtonAPI;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml;
+using MelonLoader.TinyJSON;
+using Newtonsoft.Json;
+using TogglePostProcessing.DawnRefs;
+using UnhollowerBaseLib;
+using UnityEngine;
+using UnityEngine.UI;
+using VRCSDK2;
+
+using static MelonLoader.MelonLogger;
+using Bloom = Dawn.TPP.PostProcessObjects.Bloom;
+using Object = UnityEngine.Object;
+
+namespace Dawn.TPP
+{
+    internal sealed class BuildInfo
+    {
+        internal const string Author = "arion#1223";
+        internal const string Company = null;
+        internal const string DownloadLink = "https://github.com/Arion-Kun/TogglePostProcessing/releases/";
+
+        internal const string Name = "TogglePostProcessing";
+
+        internal const string Version = "1.2.0";
+    }
+    internal sealed class Start : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Core.RegisterSettings();
+            Msg("Settings can be configured in UserData/modprefs.ini or through UIExpansionKit");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) // World Join
+        {
+            switch (buildIndex) //Prevents being called 3x
+            {
+                case 0:
+                case 1:
+                    break;
+                default:
+                    MelonCoroutines.Start(Core.WorldJoinedCoroutine());
+                    break;
+            }
+        }
+
+        internal static void OnWorldJoin()
+        {
+            Core.LayerChange();
+            
+            CustomPostProcessing.WorldJoin();
+
+            // Add extra Post Processing here | Bloom / Dark Mode
+
+        }
+
+        public override void VRChat_OnUiManagerInit()
+        { Core.m_UICreated = true;
+        #if QM
+            QuickMenu.InitQM();
+        #endif
+            
+            
+            CustomPostProcessing.CreateProcessingObjects();
+
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            Core.InternalSettingsRefresh(); 
+        #if QM
+            QuickMenu.QMPrefsRefresh();
+        #endif
+        }
+
+        public static PostProcessProfile USEMETEMPDEBUG;
+    }
+}
